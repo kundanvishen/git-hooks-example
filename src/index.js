2e2fbc7b0c126d87432523937e9a450a3a3cb8b4
Kundan Singh <kundan.singh979@gmail.com>
2017-02-07 14:29:16 +0530

    Configured lint file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Method returns true if a vowel exists in the input string. Returns false otherwise.
-
+/* jshint esversion:6 */
 String.prototype.hasVowels = function hasVowels() {
     const vowels = new RegExp('[aeiou]', 'i');
     return vowels.test(this);
--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+/* jshint esversion:6 */
+
 const expect = require('chai').expect;
 require('../src/index');
 
